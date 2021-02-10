cdce245b6b4f5a7785472a508717baebb7b8dcbd
Viktor Reshniak <revitmt@gmail.com>
2021-02-09 20:27:35 -0500

    update ex_MNIST

--- examples/ex_MNIST/run.bat
+++ examples/ex_MNIST/run.bat
@@ -1,13 +1,13 @@
 @echo off
 
 
-FOR %%m IN ( test) DO (
+FOR %%m IN (train test) DO (
 	python ex_MNIST.py --name plain --mode %%m --theta 0.0 --adiv 0.0 --piters 0
 	python ex_MNIST.py --name 1Lip  --mode %%m --theta 0.0 --adiv 0.0 --piters 1
 )
 
 FOR %%t IN (0.00 0.25 0.50 0.75 1.00) DO (
-	FOR %%m IN ( test) DO (
+	FOR %%m IN (train test) DO (
 		python ex_MNIST.py --name theta_%%t --mode %%m --theta %%t --stablim 0.2 1.0 1.2 --learn_scales --learn_shift
 	)
 )
