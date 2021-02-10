fd64d83c4d804a67d9c731a714ae8639a4c1157b
Viktor Reshniak <reshniakv@ornl.gov>
2021-02-10 11:43:38 -0500

    update ex_MNIST

--- examples/ex_MNIST/run.sh
+++ examples/ex_MNIST/run.sh
@@ -7,7 +7,7 @@ done
 
 for mode in train test; do
 	for theta in 0.10 0.25 0.50 0.75 1.00; do
-		python ex_MNIST.py --name theta_"$theta" --mode $mode --theta $theta --stablim 0.2 1.0 1.2 --learn_scales --learn_shift
+		python ex_MNIST.py --name theta_"$theta" --mode $mode --theta $theta --stablim 0.3 1.2 1.5 --learn_scales --learn_shift
 	done
 done
 
