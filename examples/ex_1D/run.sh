64ff8f6b108326f5b9b6eed1dce75a709d761747
Viktor Reshniak <revitmt@gmail.com>
2021-03-05 23:13:24 -0500

    update results

--- examples/ex_1D/run.sh
+++ examples/ex_1D/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for adiv in 0.25 0.50 0.75 1.00; do
+for adiv in 0.00 0.50 1.00 2.00 3.00; do
 	for theta in 0.00 0.25 0.50 0.75 1.00; do
 		for mode in train test; do
 			python ex_1D.py --name adiv_"$adiv"_theta_"$theta" --mode $mode --theta $theta --adiv $adiv
