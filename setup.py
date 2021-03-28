1fb4729df477ad911faa4c6b5a428e2ad5e8559b
Viktor Reshniak <revitmt@gmail.com>
2021-03-27 23:31:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
-# setup.py script to build and install patchmatch extension module written in C
-
 from distutils.core import setup
 from setuptools import find_packages
 
-# Note: setup() has access to cmd arguments of the setup.py script via sys.argv
 setup(name="implicitresnet",
 	  packages=find_packages(),
 	  package_dir={'implicitresnet': 'implicitresnet'},
 	  author='Viktor Reshniak',
       author_email='reshniakv@ornl.gov',
-      version='0.1.1'
+      version='0.1.2'
       )
