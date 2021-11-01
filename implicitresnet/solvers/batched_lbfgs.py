1583160446cccec58298aa3753a1262e642d336b
Viktor Reshniak <revitmt@gmail.com>
2021-11-01 11:12:56 -0400

    Update batched_lbfgs.py

--- implicitresnet/solvers/batched_lbfgs.py
+++ implicitresnet/solvers/batched_lbfgs.py
@@ -43,7 +43,7 @@ def _batched_cubic_interpolate(x1, f1, g1, x2, f2, g2, bounds=None):
 	return result
 
 
-def _batched_strong_wolfe(obj_func,
+def _batched_strong_wolfe_old(obj_func,
 				  x,
 				  t,
 				  d,
@@ -81,7 +81,9 @@ def _batched_strong_wolfe(obj_func,
 	batch_dim = d.size(0)
 	device    = d.device
 	d_norm    = d.abs().max(dim=1)[0]
-	g = g.clone(memory_format=torch.contiguous_format)
+
+	x = [p.clone(memory_format=torch.contiguous_format) for p in x]
+	g =  g.clone(memory_format=torch.contiguous_format)
 
 	# evaluate objective and gradient using initial step
 	f_new, g_new = obj_func(x, t, d)
@@ -184,7 +186,7 @@ def _batched_strong_wolfe(obj_func,
 		if torch.any(proceed):
 			# compute new trial value
 			t = _batched_cubic_interpolate(bracket_low, bracket_f_low, bracket_gtd_low,
-										   bracket_low, bracket_f_low, bracket_gtd_low).unsqueeze(1)
+										   bracket_high, bracket_f_high, bracket_gtd_high).unsqueeze(1)
 
 			# test that we are making sufficient progress:
 			# in case `t` is so close to boundary, we mark that we are making
@@ -253,6 +255,218 @@ def _batched_strong_wolfe(obj_func,
 	t = bracket_low.unsqueeze(1)
 	f_new = bracket_f_low
 	g_new = bracket_g_low
+	return f_new, g_new, t, ls_func_evals
+
+
+# TODO: try torch split
+def _batched_strong_wolfe(obj_func,
+				  t,
+				  d,
+				  f,
+				  g,
+				  gtd,
+				  c1=1e-4,
+				  c2=0.9,
+				  tolerance_change=1e-9,
+				  max_ls=10):
+	'''
+	A Line Search satisfying the Wolfe conditions
+
+	Ported from https://github.com/torch/optim/blob/master/lswolfe.lua and https://pytorch.org/docs/stable/_modules/torch/optim/lbfgs.html#LBFGS
+
+	Args:
+		obj_func         : a function (the objective) that takes a single input (x), the point of evaluation, and returns f(x) and df/dx
+		t                : initial step size
+		d                : descent direction
+		f                : initial function value
+		g                : gradient at initial location
+		gtd              : directional derivative at starting location
+		c1               : sufficient decrease parameter
+		c2               : curvature parameter
+		tolerance_change : minimum allowable step length
+		max_ls           : maximum nb of iterations
+
+	Output:
+		f             : function value at x+t*d
+		g             : gradient value at x+t*d
+		t             : the step length
+		ls_func_evals : the number of function evaluations
+	'''
+	batch_dim = d.size(0)
+	device    = d.device
+	d_norm    = d.abs().max(dim=1)[0]
+
+	g =  g.clone(memory_format=torch.contiguous_format)
+	t = t.squeeze(1)
+
+	# evaluate objective and gradient using initial step
+	f_new, g_new = obj_func(t); ls_func_evals = 1
+	gtd_new = (g_new*d).sum(dim=1)
+
+	#####################################################################################
+	# bracket an interval containing a point satisfying the strong Wolfe criteria
+	#####################################################################################
+	t_prev, f_prev, g_prev, gtd_prev = torch.zeros_like(t), f, g, gtd
+
+	# mask indicating which batch dimensions satisfy Wolfe criteria
+	done = torch.tensor([False]*batch_dim, dtype=torch.bool, device=device)
+	# mask indicating which batch dimensions should be bracketed
+	nobracket = torch.tensor([True]*batch_dim,  dtype=torch.bool, device=device)
+
+	# need to create buffers for bathched brackets, can this be avoided?
+	bracket     = [torch.zeros(batch_dim, dtype=t.dtype,   device=device), torch.zeros(batch_dim, dtype=t.dtype,   device=device)]
+	bracket_f   = [torch.zeros(batch_dim, dtype=f.dtype,   device=device), torch.zeros(batch_dim, dtype=f.dtype,   device=device)]
+	bracket_g   = [torch.zeros(*g.shape,  dtype=g.dtype,   device=device), torch.zeros(*g.shape,  dtype=g.dtype,   device=device)]
+	bracket_gtd = [torch.zeros(batch_dim, dtype=gtd.dtype, device=device), torch.zeros(batch_dim, dtype=gtd.dtype, device=device)]
+	ls_iter = 0
+	while ls_iter < max_ls:
+		# check conditions
+		condition = f_new > (f + c1 * t * gtd)
+		if ls_iter <= 1:
+			condition = torch.logical_and( condition, nobracket)
+		else:
+			condition = torch.logical_and( torch.logical_or( condition, f_new >= f_prev ), nobracket)
+		bracket[0][condition],     bracket[1][condition]     = t_prev[condition],   t[condition]
+		bracket_f[0][condition],   bracket_f[1][condition]   = f_prev[condition],   f_new[condition]
+		bracket_g[0][condition,:], bracket_g[1][condition,:] = g_prev[condition,:], g_new[condition,:]
+		bracket_gtd[0][condition], bracket_gtd[1][condition] = gtd_prev[condition], gtd_new[condition]
+		# batch dimensions satisfying 'condition' have been bracketed
+		nobracket[condition] = False
+		if not torch.any(nobracket):
+			break
+
+		# condition = torch.logical_and( gtd_new.abs() <= -c2 * gtd, nobracket)
+		condition = torch.logical_and( gtd_new.abs() <= c2 * gtd.abs(), nobracket)
+		bracket[0][condition],     bracket[1][condition]     = t[condition],       t[condition]
+		bracket_f[0][condition],   bracket_f[1][condition]   = f_new[condition],   f_new[condition]
+		bracket_g[0][condition,:], bracket_g[1][condition,:] = g_new[condition,:], g_new[condition,:]
+		# batch dimensions satisfying 'condition' satisfy Wolfe criteria, no need to bracket them
+		done[condition] = True
+		nobracket[condition] = False
+		if not torch.any(nobracket):
+			break
+
+		condition = torch.logical_and( gtd_new >= 0, nobracket)
+		bracket[0][condition],     bracket[1][condition]     = t_prev[condition],   t[condition]
+		bracket_f[0][condition],   bracket_f[1][condition]   = f_prev[condition],   f_new[condition]
+		bracket_g[0][condition,:], bracket_g[1][condition,:] = g_prev[condition,:], g_new[condition,:]
+		bracket_gtd[0][condition], bracket_gtd[1][condition] = gtd_prev[condition], gtd_new[condition]
+		# batch dimensions satisfying 'condition' have been bracketed
+		nobracket[condition] = False
+		if not torch.any(nobracket):
+			break
+
+		# interpolate non-bracketed dimensions
+		min_step = t + 0.01 * (t - t_prev)
+		max_step = t * 10
+		tmp = t.clone() # so that tmp and t point to different objects, and tmp is not modified
+		t[nobracket] = _batched_cubic_interpolate(  t_prev, f_prev, gtd_prev,
+													t,      f_new,  gtd_new,
+													bounds=(min_step, max_step) )[nobracket]
+
+		# next step
+		t_prev   = tmp
+		f_prev   = f_new
+		g_prev   = g_new
+		gtd_prev = gtd_new
+		f_new, g_new = obj_func(t); ls_func_evals += 1
+		gtd_new = (g_new*d).sum(1)
+		ls_iter += 1
+
+	# reached max number of iterations?
+	if ls_iter == max_ls:
+		bracket[0][nobracket],     bracket[1][nobracket]     = 0,              t[nobracket,0]
+		bracket_f[0][nobracket],   bracket_f[1][nobracket]   = f[nobracket],   f_new[nobracket]
+		bracket_g[0][nobracket,:], bracket_g[1][nobracket,:] = g[nobracket,:], g_new[nobracket,:]
+
+
+	#####################################################################################
+	# zoom phase: we now have a point satisfying the criteria, or a bracket around it.
+	# We refine the bracket until we find the exact point satisfying the criteria
+	#####################################################################################
+	def swap_low_high(condition):
+		bracket_tmp = bracket[0].clone();     bracket[0][condition],     bracket[1][condition]     = bracket[1][condition],     bracket_tmp[condition]
+		bracket_tmp = bracket_f[0].clone();   bracket_f[0][condition],   bracket_f[1][condition]   = bracket_f[1][condition],   bracket_tmp[condition]
+		bracket_tmp = bracket_g[0].clone();   bracket_g[0][condition,:], bracket_g[1][condition,:] = bracket_g[1][condition,:], bracket_tmp[condition,:]
+		bracket_tmp = bracket_gtd[0].clone(); bracket_gtd[0][condition], bracket_gtd[1][condition] = bracket_gtd[1][condition], bracket_tmp[condition]
+
+	swap_low_high(bracket_f[0] > bracket_f[1])
+
+	insuf_progress = torch.tensor([False]*batch_dim, dtype=torch.bool, device=gtd.device)
+	while not torch.all(done) and ls_iter < max_ls:
+		# line-search bracket is so small
+		proceed = (bracket[1] - bracket[0]).abs() * d_norm >= tolerance_change
+		if torch.any(proceed):
+			# compute new trial value
+			t = _batched_cubic_interpolate(bracket[0], bracket_f[0], bracket_gtd[0],
+										   bracket[1], bracket_f[1], bracket_gtd[1])
+
+			# test that we are making sufficient progress:
+			# in case `t` is so close to boundary, we mark that we are making insufficient progress, and if
+			#   + we have made insufficient progress in the last step, or
+			#   + `t` is at one of the boundary,
+			# we will move `t` to a position which is `0.1 * len(bracket)` away from the nearest boundary point.
+			bracket_max = torch.maximum(bracket[0], bracket[1])
+			bracket_min = torch.minimum(bracket[0], bracket[1])
+			eps = 0.1 * (bracket_max - bracket_min)
+			condition  = torch.minimum( bracket_max - t, t - bracket_min ) < eps
+			conditiona = torch.logical_and( proceed,  condition )
+			conditionb = torch.logical_and( proceed, ~condition )
+			if torch.any(conditiona):
+				# interpolation close to boundary
+				condition2  = torch.logical_or( insuf_progress, torch.logical_or( t >= bracket_max, t <= bracket_min ) )
+				condition2a = torch.logical_and( conditiona,  condition2 )
+				condition2b = torch.logical_and( conditiona, ~condition2 )
+				# evaluate at 0.1 away from boundary
+				if torch.any(condition2a):
+					condition3  = (t - bracket_max).abs() < (t - bracket_min).abs()
+					condition3a = torch.logical_and( condition2a,  condition3 )
+					condition3b = torch.logical_and( condition2a, ~condition3 )
+					t[condition3a] = (bracket_max - eps)[condition3a]
+					t[condition3b] = (bracket_min + eps)[condition3b]
+				insuf_progress[condition2a] = False
+				insuf_progress[condition2b] = True
+			insuf_progress[conditionb] = False
+
+			# Evaluate new point
+			f_new, g_new = obj_func(t); ls_func_evals += 1
+			gtd_new = (g_new*d).sum(1)
+			ls_iter += 1
+
+			condition  = torch.logical_or( f_new > (f + c1 * t * gtd), f_new >= bracket_f[0] )
+			conditiona = torch.logical_and( proceed,  condition )
+			conditionb = torch.logical_and( proceed, ~condition )
+			if torch.any(conditiona):
+				# Armijo condition not satisfied or not lower than lowest point
+				bracket[1][conditiona]     = t[conditiona]
+				bracket_f[1][conditiona]   = f_new[conditiona]
+				bracket_g[1][conditiona,:] = g_new[conditiona,:].clone(memory_format=torch.contiguous_format)
+				bracket_gtd[1][conditiona] = gtd_new[conditiona]
+				swap_low_high(bracket_f[0] > bracket_f[1])
+			condition2  = abs(gtd_new) <= -c2 * gtd
+			condition2a = torch.logical_and( conditionb,  condition2 )
+			condition2b = torch.logical_and( conditionb, ~condition2 )
+			# Wolfe conditions satisfied
+			done[condition2a] = True
+			condition3 = gtd_new * (bracket[1] - bracket[0]) >= 0
+			condition3 = torch.logical_and( condition2b,  condition3 )
+			# old high becomes new low
+			bracket[1][condition3]     = bracket[0][condition3]
+			bracket_f[1][condition3] = bracket_f[0][condition3]
+			bracket_g[1][condition3,:] = bracket_g[0][condition3,:]
+			bracket_gtd[1][condition3] = bracket_gtd[0][condition3]
+			# new point becomes new low
+			bracket[0][conditionb]     = t[conditionb]
+			bracket_f[0][conditionb]   = f_new[conditionb]
+			bracket_g[0][conditionb,:] = g_new[conditionb,:].clone(memory_format=torch.contiguous_format)
+			bracket_gtd[0][conditionb] = gtd_new[conditionb]
+		else:
+			break
+
+	# return stuff
+	t     = bracket[0].unsqueeze(1)
+	f_new = bracket_f[0]
+	g_new = bracket_g[0]
 	return f_new, g_new, t, ls_func_evals
 
 
@@ -352,13 +566,12 @@ class batched_LBFGS(Optimizer):
 		for p, pdata in zip(self._params, params_data):
 			p.copy_(pdata)
 
-	# def _directional_evaluate(self, closure, x, t, d):
-	#     self._add_grad(t, d)
-	#     # loss = float(closure())
-	#     loss = closure().float()
-	#     flat_grad = self._gather_flat_grad()
-	#     self._set_param(x)
-	#     return loss, flat_grad
+	def _directional_evaluate(self, closure, x, t, d):
+	    self._add_grad(t, d)
+	    loss = closure().float()
+	    flat_grad = self._gather_flat_grad()
+	    self._set_param(x)
+	    return loss, flat_grad
 
 	@torch.no_grad()
 	def step(self, closure):
@@ -524,6 +737,7 @@ class batched_LBFGS(Optimizer):
 					raise RuntimeError("only 'strong_wolfe' is supported")
 				else:
 					x_init = self._clone_param()
+					# x_init = self._params
 
 					# for i in range(batch_dim):
 					#     def obj_func(x, t, d):
@@ -545,17 +759,21 @@ class batched_LBFGS(Optimizer):
 					# print(t)
 					# exit()
 
-					def obj_func(x, t, d):
-						# self._add_grad(t, d)
-						p = self._params[0]
-						p += (t * d).view_as(p)
-						loss = closure().float()
-						flat_grad = self._gather_flat_grad()
-						self._set_param(x)
-						return loss, flat_grad
-						# return self._directional_evaluate(closure, x, t, d)
+					# def obj_func_old(x, t, d):
+					# 	self._add_grad(t, d)
+					# 	# p = self._params[0]
+					# 	# p += (t * d).view_as(p)
+					# 	loss = closure().float()
+					# 	flat_grad = self._gather_flat_grad()
+					# 	self._set_param(x)
+					# 	return loss, flat_grad
+					# 	# return self._directional_evaluate(closure, x, t, d)
 
-					loss, flat_grad, t, ls_func_evals = _batched_strong_wolfe(obj_func, x_init, t, d, loss, flat_grad, gtd)
+					def obj_func(t):
+						return self._directional_evaluate(closure, x_init, t.unsqueeze(1), d)
+
+					loss, flat_grad, t, ls_func_evals = _batched_strong_wolfe(obj_func, t, d, loss, flat_grad, gtd)
+					# loss, flat_grad, t, ls_func_evals = _batched_strong_wolfe_old(obj_func_old, x_init, t, d, loss, flat_grad, gtd)
 					# print(t)
 					# exit()
 
@@ -576,7 +794,7 @@ class batched_LBFGS(Optimizer):
 				#     loss = closure().float()
 				# flat_grad = self._gather_flat_grad()
 				# ls_func_evals += 1
-				opt_cond = flat_grad.abs().max() <= tolerance_grad
+				# opt_cond = flat_grad.abs().max() <= tolerance_grad
 			else:
 				# no line search, simply move with fixed-step
 				self._add_grad(t, d)
@@ -588,7 +806,7 @@ class batched_LBFGS(Optimizer):
 						# loss = float(closure())
 						loss = closure().float()
 					flat_grad = self._gather_flat_grad()
-					opt_cond = flat_grad.abs().max() <= tolerance_grad
+					# opt_cond = flat_grad.abs().max() <= tolerance_grad
 					ls_func_evals = 1
 
 			# update func eval
@@ -604,8 +822,7 @@ class batched_LBFGS(Optimizer):
 			if state['func_evals'] >= max_eval:
 				break
 
-			# optimal condition
-			if opt_cond:
+			if flat_grad.abs().max() <= tolerance_grad:
 				break
 
 			# print(n_iter, t.view(-1), d.abs().max(), abs(loss - prev_loss).max(), loss.max(), prev_loss.max())
