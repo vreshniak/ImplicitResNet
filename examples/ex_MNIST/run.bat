b9df896de9967807666c0b3447154b521ebc3d97
Viktor Reshniak <revitmt@gmail.com>
2021-11-01 11:06:19 -0400

    Update run.bat

--- examples/ex_MNIST/run.bat
+++ examples/ex_MNIST/run.bat
@@ -1,17 +1,22 @@
 @echo off
 
 
-FOR %%m IN (train test) DO (
-	python ex_MNIST.py --name plain --mode %%m --theta 0.0 --adiv 0.0 --piters 0
-	python ex_MNIST.py --name 1Lip  --mode %%m --theta 0.0 --adiv 0.0 --piters 1
-)
+rem FOR %%m IN (train test) DO (
+rem 	python ex_MNIST.py --name model_1_plain --model 1 --mode %%m --theta 0.0 --adiv 0.0 --piters 0 --lr 1.e-2
+rem 	rem python ex_MNIST.py --name adiv0 --mode %%m --theta 0.0 --adiv 0.0 --piters 1 --lr 1.e-2 --stablim -1.0 1.0 2.0 --learn_shift
+rem )
 
-FOR %%t IN (0.00 0.25 0.50 0.75 1.00) DO (
-	FOR %%m IN (train test) DO (
-		python ex_MNIST.py --name theta_%%t --mode %%m --theta %%t --stablim 0.3 1.2 1.5 --learn_scales --learn_shift
+
+FOR %%a IN (0.00) DO (
+	FOR %%t IN (0.20 0.30 0.40 0.50 0.75 1.00) DO (
+		FOR %%m IN (train test) DO (
+			python ex_MNIST.py --name model_1_adiv_%%a_theta_%%t --model 1 --mode %%m --theta %%t --lr 1.e-2 --adiv %%a --piters 1 --stablim 0.0 1.0 2.0 --learn_shift
+		)
 	)
 )
-
-python make_table.py
-pdflatex results.tex
-del *.aux *.log *.bak
+rem --adaugm 1.e-5
+rem --stablim 0.1 1.0 5.0 --learn_shift
+rem rem --stablim 0.2 1.0 2.0 --learn_shift --adiv 0.1
+rem python make_table.py
+rem pdflatex results.tex
+rem del *.aux *.log *.bak
