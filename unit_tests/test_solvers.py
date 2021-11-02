80dd3392f850271bd1e5781372cf7d8655879490
Viktor Reshniak <revitmt@gmail.com>
2021-11-02 17:11:04 -0400

    Update test_solvers.py

--- unit_tests/test_solvers.py
+++ unit_tests/test_solvers.py
@@ -21,41 +21,41 @@ _batches = 1
 ###############################################################################
 
 
-def get_system(name, n, dtype, device):
+def get_system(name, n, dtype, device, batches=_batches):
 	if name=='wilkinson':
 		# https://www.mathworks.com/help/matlab/ref/wilkinson.html
-		A = torch.zeros((_batches,n,n)).to(dtype=dtype, device=device)
-		for b in range(_batches):
+		A = torch.zeros((batches,n,n)).to(dtype=dtype, device=device)
+		for b in range(batches):
 			torch.diagonal(A[b,...], offset=0, dim1=0, dim2=1)[...] = torch.linspace(-(n-1)/2,(n-1)/2,n).to(dtype=dtype, device=device).abs()[...]
 		torch.diagonal(A, offset=-1, dim1=1, dim2=2)[...] = torch.ones_like(torch.diagonal(A, offset=1, dim1=1, dim2=2))[...]
 		torch.diagonal(A, offset=1,  dim1=1, dim2=2)[...] = torch.ones_like(torch.diagonal(A, offset=1, dim1=1, dim2=2))[...]
-		x  = torch.ones((_batches,n)).to(dtype=dtype, device=device)
+		x  = torch.ones((batches,n)).to(dtype=dtype, device=device)
 		# x0 = x + 0.1*torch.randn_like(x)
-		x0 = torch.rand((_batches,n)).to(dtype=dtype, device=device)
-		# x0 = torch.zeros((_batches,n)).to(dtype=dtype, device=device
+		x0 = torch.rand((batches,n)).to(dtype=dtype, device=device)
+		# x0 = torch.zeros((batches,n)).to(dtype=dtype, device=device
 		b  = torch.bmm(A,x.unsqueeze(2)).squeeze(2)
 		return lambda x:torch.bmm(A,x.unsqueeze(2)).squeeze(2), x0, b
 	if name=='laplace':
 		pi = 2*3.141592653589793238462643383279502884197169399375105820974944592307816406286
 		h = 2*pi/(n+1)
 		t = torch.linspace(h,2*pi-h,n)
-		A = torch.zeros((_batches,n,n)).to(dtype=dtype, device=device)
-		for b in range(_batches):
+		A = torch.zeros((batches,n,n)).to(dtype=dtype, device=device)
+		for b in range(batches):
 			torch.diagonal(A[b,...], offset=0, dim1=0, dim2=1)[...] = -2/h**2
 		torch.diagonal(A, offset=-1, dim1=1, dim2=2)[...] = 1/h**2
 		torch.diagonal(A, offset=1,  dim1=1, dim2=2)[...] = 1/h**2
-		x0 = torch.rand((_batches,n)).to(dtype=dtype, device=device)
-		# x0 = torch.zeros((_batches,n)).to(dtype=dtype, device=device)
+		x0 = torch.rand((batches,n)).to(dtype=dtype, device=device)
+		# x0 = torch.zeros((batches,n)).to(dtype=dtype, device=device)
 		x = torch.zeros_like(x0)
-		for i in range(_batches):
+		for i in range(batches):
 			x[i,...] = torch.sin(t)[...]
 		b = torch.bmm(A,x.unsqueeze(2)).squeeze(2)
 		return lambda x:torch.bmm(A,x.unsqueeze(2)).squeeze(2), x0, b
 	elif name=='random':
-		A  = torch.rand((_batches,n,n)).to(dtype=dtype, device=device)
-		x0 = torch.rand((_batches,n)).to(dtype=dtype, device=device)
+		A  = torch.rand((batches,n,n)).to(dtype=dtype, device=device)
+		x0 = torch.rand((batches,n)).to(dtype=dtype, device=device)
 		# x0 = torch.zeros((_batches,n)).to(dtype=dtype, device=device)
-		x  = torch.ones((_batches,n)).to(dtype=dtype, device=device)
+		x  = torch.ones((batches,n)).to(dtype=dtype, device=device)
 		b  = torch.bmm(A,x.unsqueeze(2)).squeeze(2)
 		return lambda x:torch.bmm(A,x.unsqueeze(2)).squeeze(2), x0, b
 	elif name=='mlp_jacobian':
@@ -65,12 +65,12 @@ def get_system(name, n, dtype, device):
 			torch.nn.Linear(2*n,2*n,bias=False),
 			torch.nn.ReLU(),
 			torch.nn.Linear(2*n,n,bias=False)).to(dtype=dtype, device=device)
-		input = torch.ones((_batches,n), dtype=dtype, device=device).requires_grad_(True)
+		input = torch.ones((batches,n), dtype=dtype, device=device).requires_grad_(True)
 		output = nn(input)
 		A_dot = lambda x: torch.autograd.grad(output, input, grad_outputs=x, create_graph=True, retain_graph=True, only_inputs=True)[0]
-		x0 = torch.rand((_batches,n)).to(dtype=dtype, device=device)
-		# x0 = torch.zeros((_batches,n)).to(dtype=dtype, device=device)
-		x  = torch.ones((_batches,n)).to(dtype=dtype, device=device)
+		x0 = torch.rand((batches,n)).to(dtype=dtype, device=device)
+		# x0 = torch.zeros((batches,n)).to(dtype=dtype, device=device)
+		x  = torch.ones((batches,n)).to(dtype=dtype, device=device)
 		b  = A_dot(x)
 		return A_dot, x0, b
 
@@ -116,6 +116,12 @@ class LinearSolverTest(metaclass=ABCMeta):
 		if torch.cuda.is_available():
 			self.solve(100, torch.float64, _gpu)
 
+	def test_batches_cpu_n100_float32(self):
+		self.solve(100, torch.float32, _cpu, 1)
+		self.solve(100, torch.float32, _cpu, 10)
+		self.solve(100, torch.float32, _cpu, 100)
+		self.solve(100, torch.float32, _cpu, 1000)
+
 
 ###############################################################################
 ###############################################################################
@@ -126,10 +132,10 @@ def check_convegence(method, system, error, iters, max_iters, flag):
 	assert flag==0, "%s for %12s system failed to converge after %d iterations with error %6.3e"%(method, system, iters, error)
 
 class Test_GMRES_Solver(LinearSolverTest):
-	def solve(self, n, dtype, device):
+	def solve(self, n, dtype, device, batches=_batches):
 		for system in self.systems:
-			A, x0, b = get_system(system, n, dtype, device)
-			sol, error, iters, flag = linsolve( A, x0, b, method='gmres', tol=1.e-6)
+			A, x0, b = get_system(system, n, dtype, device, batches)
+			sol, error, iters, flag = linsolve( A, x0, b, method='gmres', tol=3.e-6)
 			check_convegence('GMRES', system, error, iters, n, flag)
 
 
