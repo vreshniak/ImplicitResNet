c1b6b559407f00dbe7f559c75d81b1a0035c383c
Viktor Reshniak <revitmt@gmail.com>
2022-07-05 12:21:22 -0400

    add batch normalization layers
    Extend standard batch_norm layers to allow tracking mean and variance separately, and learning weight and bias separately

--- implicitresnet/models/misc.py
+++ implicitresnet/models/misc.py
@@ -4,8 +4,10 @@ import math
 from typing import Union
 
 import torch
-from torch.nn import Linear, ReLU, Conv2d, Module, Sequential, Parameter, ModuleList, BatchNorm1d
+from torch import Tensor
+from torch.nn import Linear, ReLU, Conv2d, Module, Sequential, Parameter, ModuleList
 from torch.nn.functional import linear
+import torch.nn.functional as F
 
 from ..utils import spectral_norm
 
@@ -45,11 +47,145 @@ def choose_activation(activation):
 
 
 ###############################################################################################
+# Normalizations
+# adapted from https://pytorch.org/docs/stable/_modules/torch/nn/modules/batchnorm.html
+
+class _BatchNorm(Module):
+	_version = 3.1415
+	__constants__ = ["track_mean", "track_variance", "momentum", "eps", "num_features", "learn_weight", "learn_bias"]
+	num_features: int
+	eps: float
+	momentum: float
+	affine: bool
+	track_running_stats: bool
+	# WARNING: weight and bias purposely not defined here.
+	# See https://github.com/pytorch/pytorch/issues/39670
+
+	def __init__( self, num_features: int, eps: float = 1e-5, momentum: float = 0.1,
+		learn_weight: bool = True, learn_bias: bool = True, track_mean: bool = True, track_variance: bool = True, device=None, dtype=None ) -> None:
+		factory_kwargs = {'device': device, 'dtype': dtype}
+		super().__init__()
+		self.num_features = num_features
+		self.eps = eps
+		self.momentum = momentum
+		self.learn_weight = learn_weight
+		self.learn_bias = learn_bias
+		self.track_mean = track_mean
+		self.track_variance = track_variance
+
+		if self.learn_weight:
+			self.weight = Parameter(torch.empty(num_features, **factory_kwargs))
+		else:
+			self.register_parameter("weight", None)
+
+		if self.learn_bias:
+			self.bias = Parameter(torch.empty(num_features, **factory_kwargs))
+		else:
+			self.register_parameter("bias", None)
+
+		self.register_buffer('running_mean', torch.zeros(num_features, **factory_kwargs))
+		# if self.track_mean:
+		# 	self.register_buffer('running_mean', torch.zeros(num_features, **factory_kwargs))
+		# else:
+		# 	self.register_buffer("running_mean", None)
+
+		self.register_buffer('running_var', torch.ones(num_features, **factory_kwargs))
+		# if self.track_variance:
+		# 	self.register_buffer('running_var', torch.ones(num_features, **factory_kwargs))
+		# else:
+		# 	self.register_buffer("running_var", None)
+
+		if self.track_mean or self.track_variance:
+			self.register_buffer('num_batches_tracked', torch.tensor(0, dtype=torch.long, **{k: v for k, v in factory_kwargs.items() if k != 'dtype'}))
+		else:
+			self.register_buffer("num_batches_tracked", None)
+
+		self.reset_parameters()
+
+	def reset_running_stats(self) -> None:
+		# running_mean/running_var/num_batches... are registered at runtime depending if self.track_mean or self.track_variance is on
+		if self.track_mean:
+			self.running_mean.zero_()  # type: ignore[union-attr]
+		if self.track_variance:
+			self.running_var.fill_(1)  # type: ignore[union-attr]
+		if self.track_mean or self.track_variance:
+			self.num_batches_tracked.zero_()  # type: ignore[union-attr,operator]
+
+	def reset_parameters(self) -> None:
+		self.reset_running_stats()
+		if self.learn_weight:
+			torch.nn.init.ones_(self.weight)
+		if self.learn_bias:
+			torch.nn.init.zeros_(self.bias)
+
+	def _check_input_dim(self, input):
+		raise NotImplementedError
+
+	def extra_repr(self):
+		return (
+			"{num_features}, eps={eps}, momentum={momentum}, learn_weight={learn_weight}, learn_bias={learn_bias}, "
+			"track_mean={track_mean}, track_variance={track_variance}".format(**self.__dict__)
+		)
+
+	def forward(self, input: Tensor) -> Tensor:
+		self._check_input_dim(input)
+
+		# exponential_average_factor is set to self.momentum (when it is available) only so that it gets updated
+		# in ONNX graph when this node is exported to ONNX.
+		if self.momentum is None:
+			exponential_average_factor = 0.0
+		else:
+			exponential_average_factor = self.momentum
+
+		if self.training and (self.track_mean or self.track_variance):
+			# TODO: if statement only here to tell the jit to skip emitting this when it is None
+			if self.num_batches_tracked is not None:  # type: ignore[has-type]
+				self.num_batches_tracked.add_(1)  # type: ignore[has-type]
+				if self.momentum is None:  # use cumulative moving average
+					exponential_average_factor = 1.0 / float(self.num_batches_tracked)
+				else:  # use exponential moving average
+					exponential_average_factor = self.momentum
+
+		r"""Buffers are only updated if they are to be tracked and we are in training mode. Thus they only need to be
+		passed when the update should occur (i.e. in training mode when they are tracked), or when buffer stats are
+		used for normalization (i.e. in eval mode when buffers are not None).
+		"""
+		return F.batch_norm(
+			input,
+			# If buffers are not to be tracked, ensure that they won't be updated
+			self.running_mean if not self.training or self.track_mean     else None,
+			self.running_var  if not self.training or self.track_variance else None,
+			self.weight,
+			self.bias,
+			self.training,
+			exponential_average_factor,
+			self.eps,
+		)
+
+class BatchNorm1d(_BatchNorm):
+	def _check_input_dim(self, input):
+		if input.dim() != 2 and input.dim() != 3:
+			raise ValueError(
+				"expected 2D or 3D input (got {}D input)".format(input.dim()))
+
+class BatchNorm2d(_BatchNorm):
+	def _check_input_dim(self, input):
+		if input.dim() != 4:
+			raise ValueError("expected 4D input (got {}D input)".format(input.dim()))
+
+class BatchNorm3d(_BatchNorm):
+	def _check_input_dim(self, input):
+		if input.dim() != 5:
+			raise ValueError("expected 5D input (got {}D input)".format(input.dim()))
+
+
+###############################################################################################
 # Perceptrons
 
 class MLP(Sequential):
 	def __init__(self, in_dim: int, out_dim: int, width: Union[int,list], depth: int,
-		activation: Union[str,list] = 'relu', bias: Union[bool,list] = True, spectral_norm=False, batch_norm=False, power_iters=1) -> None:
+		activation: Union[str,list] = 'relu', bias: Union[bool,list] = True, spectral_norm=False, batch_norm=False,
+		sn_power_iters=1, bn_track_mean: bool = True, bn_track_variance: bool = True, bn_learn_weight: bool = True, bn_learn_bias: bool = True) -> None:
 		r""" Multilayer perceptron implemented on top of torch.nn.Sequential.
 		The `depth` is the number of hidden layers, i.e., shallow network has depth 0 and the total number of layers is `depth+2`.
 		If `batch_norm` is True, it is applied to all but last layer.
@@ -76,17 +212,19 @@ class MLP(Sequential):
 
 		# batch normalization layers
 		if batch_norm:
-			bias  = [False]*(depth+1) + [bias[-1]]
-			bnorm = [ BatchNorm1d(in_dim) ] + [ BatchNorm1d(width[d]) for d in range(depth) ]
+			if bn_learn_bias:
+				bias  = [False]*(depth+1) + [bias[-1]]
+			bnorm = [ BatchNorm1d(in_dim, track_mean=bn_track_mean, track_variance=bn_track_variance, learn_weight=bn_learn_weight, learn_bias=bn_learn_bias) ] \
+				+ [ BatchNorm1d(width[d], track_mean=bn_track_mean, track_variance=bn_track_variance, learn_weight=bn_learn_weight, learn_bias=bn_learn_bias) for d in range(depth) ]
 
 		# linear layers
 		linear = [Linear(in_dim, width[0], bias=bias[0])] + [Linear(width[d], width[d+1], bias=bias[d+1]) for d in range(depth)] + [Linear(width[depth], out_dim, bias=bias[depth+1])]
 
 		# spectral normalization
 		if spectral_norm:
-			linear = [spectral_norm(l, n_power_iterations=power_iters) for l in linear]
+			linear = [spectral_norm(l, n_power_iterations=sn_power_iters) for l in linear]
 			if batch_norm:
-				bnorm = [spectral_norm(b, n_power_iterations=power_iters) for b in bnorm]
+				bnorm = [spectral_norm(b, n_power_iterations=sn_power_iters) for b in bnorm]
 
 		# batch normalization
 		if batch_norm:
